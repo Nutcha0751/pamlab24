af987f26a8afd621ca7f1767f890f52e88d3481a
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-03-07 00:22:41 +0700

    complete

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[], int size){
+	// สร้าง set เพื่อเก็บค่าที่ไม่ซ้ำกัน
+	set<int> uniqueNumbers;
+
+	// เพิ่มค่าในอาร์เรย์ลงใน set
+	for(int i = 0; i < size; i++){
+		uniqueNumbers.insert(data[i]);
+	}
+	return uniqueNumbers.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
